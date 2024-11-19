7aaf232b9b6c08ab2b4ca3374da8dbbf04bf2431
B'rynn McCormick <brynnjmccormick@gmail.com>
2024-11-18 22:11:55 -0500

    cleanup plugins and keybinds

--- lua/config.lua
+++ lua/config.lua
@@ -9,4 +9,19 @@ vim.opt.softtabstop = 2
 
 vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
 
-vim.cmd("colorscheme rose-pine")
+-- vim.cmd("colorscheme rose-pine")
+
+-- https://vonheikemen.github.io/devlog/tools/using-netrw-vim-builtin-file-explorer/
+vim.keymap.set("n", "<leader>nn", ":Lexplore %:p:h<CR>")
+vim.keymap.set("n", "<leader>na", ":Lexplore<CR>")
+vim.g.netrw_keepdir = 0
+vim.g.netrw_winsize = 25
+vim.g.netrw_banner = 0
+
+-- Telescope keybinds. Moved here because my leader wasn't correct when it was in `plugins.lua`
+local builtin = require("telescope.builtin")
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help flags' })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -31,19 +31,19 @@ require("lazy").setup({
       })
     end
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
   "williamboman/mason.nvim",
   {
-    "neovim/nvim-lspconfig",
+    "hrsh7th/nvim-cmp",
     dependencies = {
-      {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = { "williamboman/mason.nvim" },
-      },
-      "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
-      "hrsh7th/nvim-cmp",
     },
     config = function()
       local cmp = require("cmp")
@@ -64,6 +64,18 @@ require("lazy").setup({
           { name = "nvim_lsp" }
         }
       })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+      },
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
       local lspconfig = require("lspconfig")
       local mason = require("mason")
       local mason_lspconfig = require("mason-lspconfig")
@@ -72,12 +84,39 @@ require("lazy").setup({
       mason.setup()
       mason_lspconfig.setup()
 
-      lspconfig.ts_ls.setup({})
-      lspconfig.rust_analyzer.setup({})
-      lspconfig.lua_ls.setup({})
-      lspconfig.html.setup({})
+      lspconfig.ts_ls.setup({
+        capabilties = capabilities,
+      })
+      lspconfig.rust_analyzer.setup({
+        capabilties = capabilities,
+      })
+      lspconfig.lua_ls.setup({
+        capabilties = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" }
+            }
+          }
+        }
+      })
+      lspconfig.html.setup({
+        capabilties = capabilities,
+      })
       lspconfig.emmet_language_server.setup({
         capabilties = capabilities,
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        -- https://neovim.discourse.group/t/pyright-can-not-find-root-dir-single-file-mode/2156
+        root_dir = function(fname)
+          local util = require('lspconfig/util')
+          local root_files = {
+            'pyrightconfig.json'
+          }
+          return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or
+          util.path.dirname(fname)
+        end
       })
     end
   },
